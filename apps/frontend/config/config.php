6cca9813c4c87fd622c28ecfd439d6394985e609
Jon Hinks <projects.ws@jondh.me.uk>
2018-09-09 18:33:27 +0100

    PHP7 no longer has any magic quotes stuff

--- apps/frontend/config/config.php
+++ apps/frontend/config/config.php
@@ -3,6 +3,9 @@
 // include project configuration
 include(SF_ROOT_DIR.DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.'config.php');
 
+// No longer available in PHP 7 :-)
+function set_magic_quotes_runtime() {}
+
 // symfony bootstraping
 require_once($sf_symfony_lib_dir.'/util/sfCore.class.php');
 sfCore::bootstrap($sf_symfony_lib_dir, $sf_symfony_data_dir);
